e07f2136b9b6bb4e358f011ee5cff7286ceec6f3
Lainezz <diegolinaresmojacar@gmail.com>
2022-12-01 17:27:52 +0100

    Enunciado ejer clase mejorado

---
+++ PythonUnittest/src/IntroUnitTest/EjercicioClase/get_partes.py
@@ -0,0 +1,22 @@
+"""
+1.	Implementa dos funciones para obtener la parte entera y la parte decimal de un 
+número en punto flotante (double). La definición de las funciones es como sigue: 
+•	def get_parte_entera(numero) return parte entera del numero
+•	def get_parte_decimal(numero) return parte decimal del numero 
+"""
+
+"""
+1º Debéis hacer una tabla, con los casos de prueba que vayáis a realizar
+2º Realizáis la implementación del código
+3º Implementáis los casos de prueba que hayáis definido en la parte 1
+4º Ejecutais los casos de prueba
+5º Comprobáis los resultados
+6º Arregláis los fallos y volvéis a ejecutar las pruebas
+"""
+
+"""
+Tabla:
+Función a probar: get_parte_entera
+¿Qué compruebo?     Val.Entrada     Res.Esperado    Res.Obtenido
+assertEqual()       1.02            1               ?
+"""
